67e873be1ad9ad64015ce20c4997b813eb146687
bibaqi <billynekem@gmail.com>
2024-05-19 18:15:35 +0700

    edit

--- Program/billy_baihaqi/index.php
+++ Program/billy_baihaqi/index.php
@@ -12,7 +12,7 @@
         <table border="1" cellspacing="0" cellpadding="10">
             <tr>
                 <th><a href="login_ui_billy.php">File</a></th>
-                <th><a href="master_billy.php">Master</a></th>
+                <th><a href="master_billy.php">Utama</a></th>
                 <th><a href="shift_billy.php">Kasir</a></th>
                 <th><a href="penjualan_billy.php">Penjualan</a></th>
             </tr>
